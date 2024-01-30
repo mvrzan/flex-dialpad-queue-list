8412dff215f76849594a58d396635cdf184c84ea
mvrzan <mvrzan@icloud.com>
2024-01-30 16:12:06 -0500

    delete the generic comment

--- flex-dialpad-queue-list/src/FlexDialpadQueueListPlugin.tsx
+++ flex-dialpad-queue-list/src/FlexDialpadQueueListPlugin.tsx
@@ -11,12 +11,6 @@ export default class FlexDialpadQueueListPlugin extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof Flex }
-   */
   async init(flex: typeof Flex, manager: Flex.Manager): Promise<void> {
     // pass the PasteThemeProvider to all Flex UI components without the need to wrap them separately
     flex.setProviders({
